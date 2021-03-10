669d6e3347765c834f384dbf873bca1f7a358f64
Nico8-up <80417806+Nico8-up@users.noreply.github.com>
2021-03-10 19:13:55 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,35 +1,40 @@
 input.onButtonPressed(Button.A, function () {
     basic.showIcon(IconNames.Happy)
 })
-input.onButtonPressed(Button.B, function () {
+input.onButtonPressed(Button.AB, function () {
     basic.showArrow(ArrowNames.North)
 })
+input.onButtonPressed(Button.B, function () {
+    basic.showString("GEMMA")
+})
 basic.forever(function () {
-    if (input.isGesture(Gesture.ScreenDown)) {
+    if (input.isGesture(Gesture.LogoUp)) {
         music.playMelody("C5 C C5 C C5 C C5 C ", 129)
     }
-    basic.showLeds(`
-        . . . . .
-        . # . # .
-        . . . . .
-        # . . . #
-        # # # # #
-        `)
-    basic.pause(200)
-    basic.showLeds(`
-        . . . . .
-        . # . # .
-        . . . . .
-        # # # # #
-        . . . . .
-        `)
-    basic.pause(200)
-    basic.showLeds(`
-        . . . . .
-        . # . # .
-        . . . . .
-        # # # # #
-        # . . . #
-        `)
-    basic.pause(200)
+    if (input.isGesture(Gesture.LogoDown)) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . . . .
+            # . . . #
+            # # # # #
+            `)
+        basic.pause(200)
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . . . .
+            # # # # #
+            . . . . .
+            `)
+        basic.pause(200)
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . . . .
+            # # # # #
+            # . . . #
+            `)
+        basic.pause(200)
+    }
 })
